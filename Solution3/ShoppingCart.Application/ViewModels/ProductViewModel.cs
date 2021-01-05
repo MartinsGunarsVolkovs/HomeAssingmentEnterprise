767249fa4dd4f88f6c74bcc8286bee694d4aafca
Mārtiņš Gunārs Volkovs <martins.volkovs.f36427@mcast.edu.mt>
2021-01-05 19:04:52 +0100

    Added input validation for ProductViewModel

--- Solution3/ShoppingCart.Application/ViewModels/ProductViewModel.cs
+++ Solution3/ShoppingCart.Application/ViewModels/ProductViewModel.cs
@@ -14,11 +14,11 @@ namespace ShoppingCart.Application.ViewModels
         [Required(ErrorMessage = "Please input a price")]
         [Range(typeof(double), "0", "9999", ErrorMessage = "{0} must be a decimal/number between {1} and {2}.")]
         public double Price { get; set; }
-
+        [Required(ErrorMessage ="Please input a description")]
         public string Description { get; set; }
-      
+        [Required(ErrorMessage ="Category must be selected")]
         public CategoryViewModel Category { get; set; }
-
+        [Required(ErrorMessage ="Please add an image")]
         public string ImageUrl { get; set; }
 
         public bool Disable { get; set; }
